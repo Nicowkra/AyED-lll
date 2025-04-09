213f376ccf80fc0f77a3d420462527d0c80f3d6e
Nico <nicowald@Nicos-MacBook-Pro.local>
2025-04-09 16:14:11 -0300

    Comentado

--- TP 1/Ej2.cpp
+++ TP 1/Ej2.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 
 using namespace std;
-int calcPos (string s ){
+int calcPos (string s ){ //calcula la posicion final del string
     int pos = 0;
     for (char c:s){
         if(c=='+'){pos++;}
@@ -13,22 +13,22 @@ int calcPos (string s ){
     }
     return pos;
 }
-int incog (string s){
+int incog (string s){ //calcula la cantidad de incognitas y devuelve 2^(cantidad de incognitas)
     int incog = 0;
     for (char c:s){
         if(c=='?'){incog++;}
     }
     return pow(2,incog);
 }
-int posibilidades (string s,int i,int posActual, int posFinal){
-if (i == s.size()){
+int posibilidades (string s,int i,int posActual, int posFinal){ //funcion recursiva te devuelve todos los caminos correctos
+if (i == s.size()){ //caso base devuelve 1 si es un fin correcto o 0 si esta mal
     if (posActual == posFinal){return 1;}
     else{return 0;}
 }
-if(s[i] == '+'){return posibilidades(s,i+1,posActual+1,posFinal);}
-else if (s[i] == '-'){return posibilidades(s,i+1,posActual-1,posFinal);}
-else{
-    int posibleMas = posibilidades(s,i+1,posActual+1,posFinal);
+if(s[i] == '+'){return posibilidades(s,i+1,posActual+1,posFinal);} //avanza una posicion e itera al proximo
+else if (s[i] == '-'){return posibilidades(s,i+1,posActual-1,posFinal);} // retrocede una posicion e itera al proximo
+else{ // es el caso que hay un ?, entonces itera las dos posibilidades y las suma
+    int posibleMas = posibilidades(s,i+1,posActual+1,posFinal); 
     int posibleMenos = posibilidades(s,i+1,posActual-1,posFinal);
     return posibleMas + posibleMenos;
 }
@@ -50,7 +50,7 @@ int posFinal = calcPos(s1);
 int incognitas = incog(s2);
 int posibles = posibilidades(s2,0,0,posFinal);
 
-double probabilidad = (double)posibles/incognitas;
+double probabilidad = (double)posibles/incognitas; // hago los resultados correctos/cantidad de posibles caminos
 
 cout << fixed;
 cout.precision(12);
