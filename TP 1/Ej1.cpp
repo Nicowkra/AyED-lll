14ba3e8f01b37de94a0e6fe41cf1949e2a59d427
Nicolás Wald <99031285+Nicowkra@users.noreply.github.com>
2025-04-09 16:03:12 -0300

    Add files via upload

---
+++ TP 1/Ej1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int i = 0;
+    int casos;
+    cin >> casos;
+    while (casos>i){
+        i++;
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+    }
+}
